5c0e3e194de51ac935bfe40a7d3421b1b3a408f4
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-12-15 09:54:05 +0700

    feat: add fragment to navigate between page

--- app/src/main/java/com/michael/dormie/activity/MasterActivity.java
+++ app/src/main/java/com/michael/dormie/activity/MasterActivity.java
@@ -1,6 +1,7 @@
 package com.michael.dormie.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -12,6 +13,12 @@ import com.google.android.material.color.DynamicColors;
 
 import com.google.android.material.navigation.NavigationView;
 import com.michael.dormie.R;
+import com.michael.dormie.fragment.ChatFragment;
+import com.michael.dormie.fragment.HomeFragment;
+import com.michael.dormie.fragment.ProfileFragment;
+import com.michael.dormie.fragment.RentalRegistrationFragment;
+import com.michael.dormie.fragment.SettingFragment;
+import com.michael.dormie.utils.NavigationUtil;
 
 public class MasterActivity extends AppCompatActivity {
 
@@ -19,11 +26,19 @@ public class MasterActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
 
+    private final HomeFragment homeFragment = HomeFragment.newInstance("", "");
+    private final ChatFragment chatFragment = ChatFragment.newInstance("", "");
+    private final RentalRegistrationFragment rentalRegistrationFragment = RentalRegistrationFragment.newInstance("", "");
+    private final ProfileFragment profileFragment = ProfileFragment.newInstance("", "");
+    private final SettingFragment settingFragment = SettingFragment.newInstance("", "");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
         DynamicColors.applyToActivitiesIfAvailable(this.getApplication());
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
         initUI();
         initUIAction();
     }
@@ -38,6 +53,7 @@ public class MasterActivity extends AppCompatActivity {
         topAppBar.setNavigationOnClickListener(this::handleNavigationOnClick);
         navigationView.setNavigationItemSelectedListener(this::handleNavigationItemSelected);
         navigationView.setCheckedItem(R.id.home_page);
+        NavigationUtil.changeFragment(this, R.id.activity_master_fl, homeFragment);
     }
 
     private void handleNavigationOnClick(View view) {
@@ -49,14 +65,19 @@ public class MasterActivity extends AppCompatActivity {
         drawerLayout.close();
         switch (item.getItemId()) {
             case R.id.home_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, homeFragment);
                 return true;
             case R.id.chat_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, chatFragment);
                 return true;
             case R.id.rental_registration_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, rentalRegistrationFragment);
                 return true;
             case R.id.profile_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, profileFragment);
                 return true;
             case R.id.setting_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, settingFragment);
                 return true;
         }
         return false;
---
+++ app/src/main/java/com/michael/dormie/utils/RequestSignal.java
@@ -0,0 +1,7 @@
+package com.michael.dormie.utils;
+
+public class RequestSignal {
+    // todo: add request signal here with the code
+    public final static int TEMPLATE_FORMAT = 0;
+    private RequestSignal() {}
+}
