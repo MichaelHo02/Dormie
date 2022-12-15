3d6822876c85f33167719bff06e7e7af5200752d
Hồ Lê Minh Thạch <67449167+MichaelHo02@users.noreply.github.com>
2022-12-15 10:42:45 +0700

    [Feature] Add master layout (#3)
    * feat: Add material 3 theme
    
    * feat: Add drawer navigation
    
    * fix: theme issue
    
    * feat: add top bar component
    
    * feat: add fragment to navigate between page

--- app/src/main/java/com/michael/dormie/activity/MasterActivity.java
+++ app/src/main/java/com/michael/dormie/activity/MasterActivity.java
@@ -1,17 +1,85 @@
 package com.michael.dormie.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.color.DynamicColors;
+
+import com.google.android.material.navigation.NavigationView;
 import com.michael.dormie.R;
+import com.michael.dormie.fragment.ChatFragment;
+import com.michael.dormie.fragment.HomeFragment;
+import com.michael.dormie.fragment.ProfileFragment;
+import com.michael.dormie.fragment.RentalRegistrationFragment;
+import com.michael.dormie.fragment.SettingFragment;
+import com.michael.dormie.utils.NavigationUtil;
 
 public class MasterActivity extends AppCompatActivity {
+
+    private MaterialToolbar topAppBar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private final HomeFragment homeFragment = HomeFragment.newInstance("", "");
+    private final ChatFragment chatFragment = ChatFragment.newInstance("", "");
+    private final RentalRegistrationFragment rentalRegistrationFragment = RentalRegistrationFragment.newInstance("", "");
+    private final ProfileFragment profileFragment = ProfileFragment.newInstance("", "");
+    private final SettingFragment settingFragment = SettingFragment.newInstance("", "");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master);
+        DynamicColors.applyToActivitiesIfAvailable(this.getApplication());
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
+        initUI();
+        initUIAction();
+    }
+
+    private void initUI() {
+        topAppBar = findViewById(R.id.activity_master_top_bar);
+        drawerLayout = findViewById(R.id.activity_master_drawer_layout);
+        navigationView = findViewById(R.id.activity_master_navigation);
+    }
+
+    private void initUIAction() {
+        topAppBar.setNavigationOnClickListener(this::handleNavigationOnClick);
+        navigationView.setNavigationItemSelectedListener(this::handleNavigationItemSelected);
+        navigationView.setCheckedItem(R.id.home_page);
+        NavigationUtil.changeFragment(this, R.id.activity_master_fl, homeFragment);
+    }
+
+    private void handleNavigationOnClick(View view) {
+        drawerLayout.open();
+    }
+
+    private boolean handleNavigationItemSelected(MenuItem item) {
+        item.setChecked(true);
+        drawerLayout.close();
+        switch (item.getItemId()) {
+            case R.id.home_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, homeFragment);
+                return true;
+            case R.id.chat_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, chatFragment);
+                return true;
+            case R.id.rental_registration_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, rentalRegistrationFragment);
+                return true;
+            case R.id.profile_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, profileFragment);
+                return true;
+            case R.id.setting_page:
+                NavigationUtil.changeFragment(this, R.id.activity_master_fl, settingFragment);
+                return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/michael/dormie/utils/NavigationUtil.java
@@ -0,0 +1,40 @@
+package com.michael.dormie.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+public class NavigationUtil {
+    public static void navigateActivity(Activity activity, Context context, Class<?> cls , int reqCode, Bundle bundle) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtras(bundle);
+        activity.startActivityForResult(intent, reqCode);
+    }
+
+    public static void navigateActivity(Fragment fragment, Context context, Class<?> cls, int reqCode) {
+        Intent intent = new Intent(context, cls);
+        fragment.startActivityForResult(intent, reqCode);
+    }
+
+    public static void changeFragment(FragmentActivity fragmentActivity, @IdRes int containerViewId, @NonNull Fragment fragment) {
+        fragmentActivity
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerViewId, fragment)
+                .commit();
+    }
+
+    public static void removeFragment(FragmentActivity fragmentActivity, @NonNull Fragment fragment) {
+        fragmentActivity
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .remove(fragment)
+                .commit();
+    }
+}
