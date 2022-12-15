0e53dd2831ffa6a69c700016c984ca303578eb9b
Quan Le <54403788+yukiriosu@users.noreply.github.com>
2022-12-15 20:37:18 +0700

    [Feature] SignIn and SignUp UI first 2 activities (#4)
    * Update .gitignore
    
    * Squashed commit of the following:
    
    commit ef8b59074f9d531aa7a9bb2ac905f0889f74a771
    Author: Hồ Lê Minh Thạch <67449167+MichaelHo02@users.noreply.github.com>
    Date:   Wed Dec 14 23:35:58 2022 +0700
    
        [Feature] Config master layout (#1)
    
        * feat: Add material 3 theme
    
        * feat: Add drawer navigation
    
        * feat: Add drawer navigation
    
    * Update build.gradle
    
    * Update few UI
    
    * Sign In UI
    
    * SignIn and SignUp Activity
    
    * Fix according to Thach
    
    * Add password something

---
+++ app/src/main/java/com/michael/dormie/activity/SignUpActivity.java
@@ -0,0 +1,34 @@
+package com.michael.dormie.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.michael.dormie.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    TextInputLayout username, password, confirmPassword;
+    Button signUpButton;
+    TextView signInButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        initVariables();
+    }
+
+    private void initVariables() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        signInButton = findViewById(R.id.signInButton);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        signInButton.setOnClickListener(v -> finish()); // Return to sign in activity
+    }
+}
