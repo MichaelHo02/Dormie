8a3d80975ea712cd9e85297ade0d4c84d89030d3
Yukiri <leminhquan310@gmail.com>
2022-12-20 13:07:18 +0700

    Im tired

--- app/src/main/java/com/michael/dormie/activity/TenantSignUpForm.java
+++ app/src/main/java/com/michael/dormie/activity/TenantSignUpForm.java
@@ -14,6 +14,7 @@ public class TenantSignUpForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenant_sign_up_form);
+        initVariables();
     }
 
     private void initVariables() {
