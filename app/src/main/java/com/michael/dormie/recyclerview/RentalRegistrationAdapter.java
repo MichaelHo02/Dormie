31f2c6576d16db630d315056ee4ab6282316a6f8
DarcieNg <s3865443@rmit.edu.vn>
2022-12-21 12:21:17 +0700

    feature/rental_registration_UI_2

---
+++ app/src/main/java/com/michael/dormie/recyclerview/RentalRegistrationAdapter.java
@@ -0,0 +1,61 @@
+package com.michael.dormie.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.michael.dormie.R;
+
+import java.util.List;
+
+public class RentalRegistrationAdapter extends RecyclerView.Adapter<RentalRegistrationAdapter.RentalRegistrationViewHolder>{
+
+    private RentalRegistrationRecyclerInterface recyclerInterface;
+    private List<RentalRegistrationCard> list;
+
+    @NonNull
+    @Override
+    public RentalRegistrationAdapter.RentalRegistrationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.request_card, parent, false);
+        return new RentalRegistrationAdapter.RentalRegistrationViewHolder(view, recyclerInterface);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RentalRegistrationAdapter.RentalRegistrationViewHolder holder, int position) {
+        RentalRegistrationCard card = list.get(position);
+        if (card == null) {
+            return;
+        }
+        holder.cardTitle.setText(card.getCardTitle());
+        holder.cardContent.setText(card.getCardContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class RentalRegistrationViewHolder extends RecyclerView.ViewHolder {
+        private TextView cardTitle;
+        private TextView cardContent;
+        private ImageButton cardButton;
+
+        public RentalRegistrationViewHolder(@NonNull View itemView, RentalRegistrationRecyclerInterface recyclerInterface) {
+            super(itemView);
+
+            cardTitle = itemView.findViewById(R.id.registration_title);
+            cardContent = itemView.findViewById(R.id.registration_content);
+            cardButton = itemView.findViewById(R.id.registration_check);
+
+            cardButton.setOnClickListener(this::handleRegistrationCheckClick);
+        }
+
+        private void handleRegistrationCheckClick(View view) {
+        }
+    }
+}
