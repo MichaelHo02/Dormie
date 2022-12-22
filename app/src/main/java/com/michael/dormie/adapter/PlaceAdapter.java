81efdd19bde10d286739f43d95096d4a76a6348f
Hồ Lê Minh Thạch <67449167+MichaelHo02@users.noreply.github.com>
2022-12-22 21:47:17 +0700

    [Feature] Home page UI & Additional Refactoring (#5)
    * refactor: change the layout to correct design
    
    * refactor: update static string
    
    * feature: update the navigation between sign in and master layout
    
    * feat: add flash screen
    
    * feat: update card render list for homepage
    
    * feat: update card bottom bar and logic for home fragment
    
    * feat: update create card and bottom bar

---
+++ app/src/main/java/com/michael/dormie/adapter/PlaceAdapter.java
@@ -0,0 +1,46 @@
+package com.michael.dormie.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.michael.dormie.R;
+import com.michael.dormie.model.Place;
+
+import java.util.List;
+
+public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.ItemHolder> {
+    private List<Place> places;
+
+    public PlaceAdapter(List<Place> places) {
+        this.places = places;
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.place_holder_item, parent, false);
+        return new ItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return places != null ? places.size() : 0;
+    }
+
+    public static final class ItemHolder extends RecyclerView.ViewHolder {
+
+        public ItemHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/michael/dormie/fragment/HomeFragment.java
+++ app/src/main/java/com/michael/dormie/fragment/HomeFragment.java
@@ -2,13 +2,27 @@ package com.michael.dormie.fragment;
 
 import android.os.Bundle;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.michael.dormie.R;
+import com.michael.dormie.activity.FlashScreenActivity;
+import com.michael.dormie.activity.PostCreationActivity;
+import com.michael.dormie.adapter.PlaceAdapter;
+import com.michael.dormie.model.Place;
+import com.michael.dormie.utils.NavigationUtil;
+import com.michael.dormie.utils.RequestSignal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,9 +30,6 @@ import com.michael.dormie.R;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -26,19 +37,16 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private View view;
+    private MaterialToolbar topAppBar;
+    private RecyclerView recyclerView;
+    private FloatingActionButton addBtn;
+    private List<Place> places;
+    private PlaceAdapter placeAdapter;
+
     public HomeFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -61,6 +69,44 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        initUI();
+        return view;
+    }
+
+    private void initUI() {
+        topAppBar = view.findViewById(R.id.fragment_home_top_bar);
+        recyclerView = view.findViewById(R.id.fragment_home_rv);
+        addBtn = view.findViewById(R.id.fragment_home_float_action);
+        topAppBar.setNavigationOnClickListener(this::handleNavigationOnClick);
+        recycleViewInit();
+        addBtn.setOnClickListener(this::handleBAddBtnOnClick);
+    }
+
+    private void handleBAddBtnOnClick(View view) {
+        Bundle bundle = new Bundle();
+        NavigationUtil.navigateActivity(this, this.requireContext(), PostCreationActivity.class, RequestSignal.TEMPLATE_FORMAT, bundle);
+    }
+
+    private void handleNavigationOnClick(View view) {
+        DrawerLayout drawerLayout = view.getRootView().findViewById(R.id.activity_master_drawer_layout);
+        drawerLayout.open();
+    }
+
+
+    private void recycleViewInit() {
+        LinearLayoutManager manager = new LinearLayoutManager(requireContext());
+        recyclerView.setLayoutManager(manager);
+        places = getPlaceList();
+        placeAdapter = new PlaceAdapter(places);
+        recyclerView.setAdapter(placeAdapter);
+    }
+
+    private List<Place> getPlaceList() {
+        List<Place> places = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            places.add(new Place());
+        }
+        return places;
     }
 }
