43c355f5a1c00d91319f6d520b26370952f98ca3
DarcieNg <s3865443@rmit.edu.vn>
2023-01-14 15:26:02 +0700

    feature/Detail view update 2

--- app/src/main/java/com/michael/dormie/fragment/DetailFragment.java
+++ app/src/main/java/com/michael/dormie/fragment/DetailFragment.java
@@ -1,4 +1,78 @@
 package com.michael.dormie.fragment;
 
-public class DetailFragment {
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.michael.dormie.R;
+import com.michael.dormie.databinding.FragmentDetailBinding;
+
+public class DetailFragment extends Fragment {
+    private static final String TAG = "DetailFragment";
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    FragmentDetailBinding b;
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+
+    private View view;
+    private MaterialToolbar topAppBar;
+
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        b = FragmentDetailBinding.inflate(inflater, container, false);
+        return b.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        b = null;
+    }
 }
