8b68004cc74490f7e274154dfead66515b7c7ebb
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2023-01-16 09:43:49 +0700

    fix: small amount of loading only

--- app/src/main/java/com/michael/dormie/HomeTenantFragment.java
+++ app/src/main/java/com/michael/dormie/HomeTenantFragment.java
@@ -56,7 +56,7 @@ public class HomeTenantFragment extends Fragment {
 
     private boolean isLoading;
     private boolean isLastPage;
-    private int totalPage = 4;
+    private int totalPage = 2;
     private int currentPage = 1;
 
     @Override
