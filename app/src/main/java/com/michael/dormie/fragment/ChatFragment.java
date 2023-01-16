4bf92c7a2b15293ed38b0df8ffacb65ce8637c8c
AshtonPh <69978620+AshtonPh@users.noreply.github.com>
2023-01-16 21:48:39 +0700

    Chat Fragment + Chat Message + Message
    Core of chat feature

--- app/src/main/java/com/michael/dormie/fragment/ChatFragment.java
+++ app/src/main/java/com/michael/dormie/fragment/ChatFragment.java
@@ -4,11 +4,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.michael.dormie.R;
+import com.michael.dormie.model.ChatMessage;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,18 +40,15 @@ public class ChatFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private View view;
+
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
+
     public ChatFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static ChatFragment newInstance(String param1, String param2) {
         ChatFragment fragment = new ChatFragment();
@@ -61,6 +72,62 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this.requireActivity(), gso);
+
+        // Display current messages
+        displayChatMessages();
+
+        // Using FAB to send message
+        FloatingActionButton fab = (FloatingActionButton)getView().findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)getView().findViewById(R.id.input);
+                // Read the input field and push a new instance of Chat to the Firebase database
+                FirebaseDatabase database = FirebaseDatabase.getInstance("https://dormie-5ad44-default-rtdb.asia-southeast1.firebasedatabase.app");
+                DatabaseReference myRef = database.getReference("server/saved_chat");
+                myRef.push().setValue(new ChatMessage(input.getText().toString(), FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+                // Clear current input
+                input.setText("");
+            }
+        });
+        return view;
     }
+
+    private void displayChatMessages() {
+
+        // Currently using temporary database for chat
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://dormie-5ad44-default-rtdb.asia-southeast1.firebasedatabase.app");
+        DatabaseReference myRef = database.getReference("server/saved_chat/");
+
+        // Use FirebaseListOptions to obtain database
+        FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
+                .setQuery(myRef, ChatMessage.class)
+                .setLayout(R.layout.message)
+                .setLifecycleOwner(this)
+                .build();
+
+        // User Firebase List Adapter to populate chat data
+        FirebaseListAdapter<ChatMessage> adapter = new FirebaseListAdapter<ChatMessage>(options) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                // Obtain references for message, time, and user
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+                // Change message text, user, and time
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+            }
+        };
+        ListView listOfMessages = (ListView)getView().findViewById(R.id.list_of_messages);
+        listOfMessages.setAdapter(adapter);
+
+    };
+
+    private FirebaseListAdapter<ChatMessage> adapter;
 }
